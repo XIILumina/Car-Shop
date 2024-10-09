8259e5b09b11d18dbe8d2d10cb2442df0a5d1bd9
Alberts <ipa22.a.zibrevs@vtdt.edu.lv>
2024-10-09 11:20:05 +0300

    minor work with setting tailwind

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- resources/views/partials/footer.blade.php -->
+
+<footer class="bg-gray-800 text-white text-center py-4">
+    <p>&copy; {{ date('Y') }} My Laravel App. All rights reserved.</p>
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/partials/navbar.blade.php -->
+
+<nav class="bg-blue-600 text-white p-4">
+    <div class="container mx-auto">
+        <h1 class="text-2xl font-bold">My Laravel App</h1>
+        <ul class="flex space-x-4">
+            <li><a href="/" class="hover:underline">Home</a></li>
+            <li><a href="/about" class="hover:underline">About</a></li>
+            <li><a href="/contact" class="hover:underline">Contact</a></li>
+        </ul>
+    </div>
+</nav>
